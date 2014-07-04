3b19d4c4227e366c5ef4b5dcb8521c60c7b02503
Piotr Gasiorowski <WooDzu@users.noreply.github.com>
2014-07-04 16:09:18 +0100

    Use batchUnsubscribe, logresults and drop the HTML form

--- bulk-delete.php
+++ bulk-delete.php
@@ -1,95 +1,83 @@
-<?
-include 'header.php';
-?>
 <?php
-// Functions to do actions
 
-function pleasedelete($email) {
-/**
-This Example shows how to pull the Info for a Member of a List using the MCAPI.php 
-class and do some basic error checking.
-**/
-require_once 'inc/MCAPI.class.php';
-require_once 'inc/config.inc.php'; //contains apikey
+die('Unlock first');
 
-$api = new MCAPI($_REQUEST["apikey"]);
+//const EMAILS_FILE = 'test.csv';
+const EMAILS_FILE = 'emails.csv';
+const API_KEY = '';
+const LIST_ID = '';
 
-$retval = $api->listUnsubscribe( $_REQUEST["listid"],$email,true,false,false);
-if ($api->errorCode){
-    echo "Unable to load listUnsubscribe()!\n";
-	echo "\tCode=".$api->errorCode."\n";
-	echo "\tMsg=".$api->errorMessage."\n";
-} else {
-    echo "Returned: ".$retval."\n";
+function pleasedelete(array $emails) {
+    /**
+    This Example shows how to pull the Info for a Member of a List using the MCAPI.php 
+    class and do some basic error checking.
+    **/
+    require_once 'inc/MCAPI.class.php';
+    require_once 'inc/config.inc.php'; //contains apikey
+    
+    $api = new MCAPI(API_KEY);
+
+    // $retval = $api->listUnsubscribe( $_REQUEST["listid"],$email,true,false,false);
+    // $retval = 'todo: batchUnsubscribe';
+    $retval = $api->listBatchUnsubscribe(LIST_ID, $emails, true, false, false);
+    // todo: batchUnsubscribe
+
+    if ($api->errorCode){
+        echo "Unable to load listUnsubscribe()!\n";
+    	echo "\tCode=".$api->errorCode."\n";
+    	echo "\tMsg=".$api->errorMessage."\n";
+        return false;
+    } else {
+        var_dump($retval);
+    }
+    return true;
 }
 
+if (!file_exists(EMAILS_FILE)) { // I got this code somewhere ...
+    die("Error: File does no exists<br />"); 
 }
 
+$row = 0;
+$batch = 0;
+$emails = array();
 
+if (($handle = fopen(EMAILS_FILE, "r")) !== FALSE) {
 
+    // run forever
+    set_time_limit(0);
 
-// Upload a file with 2 columns, full number, status
-if ($_GET["mcboogie"] == "woogie") { //  form was submitted
-	
-
-if ($_FILES["file"]["error"] > 0) // I got this code somewhere ...
-  {
-  echo "Error: " . $_FILES["file"]["error"] . "<br />"; 
-  }
-else
-  {
-
- if (($handle = fopen($_FILES["file"]["tmp_name"], "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) { // $Data = row data
+            
+        $row++;
 
 		// Get the email address
-		$email = $data[0];
-		
-		if ($_REQUEST["delete"] == "true") {
-		
-		pleasedelete($email);
-		
-		}
-		
-		echo $email . " - deleted <br />";
-		
-		$row++;
+		$emails[] = $data[0];
+
+        if ($row == 1000) {
+            ob_start();
+		    pleasedelete($emails);
+            $buff = ob_get_clean();
+
+            $batch++;
+
+            $l = "\n\n---------------BATCH $batch -------------------\n\n";
+
+            file_put_contents(__DIR__.'/log.log',  $l . $buff. PHP_EOL, FILE_APPEND);
+            $row = 0;
+            $emails = array();
+        }
     }
-	
 
-  }
+    // Process last batch
+	ob_start();
+	pleasedelete($emails);
+    $buff = ob_get_clean();
+    $batch++;
 
-	
+    $l = "\n\n--------------LAST BATCH $batch -------------------\n\n";
+
+    file_put_contents(__DIR__.'/log.log',  $l . $buff. PHP_EOL, FILE_APPEND);
 }
-	
-	} else {
-// Show form for uploading
-	
-	?>
-
-
-<h1>MailChimp tools - Bulk Delete</h1>
-<p>Import a list of email addresses (in a text file) - then click 'delete' and this will delete the whole batch of email users from your mailchimp list.<br/><br/><strong>There will be no 'are you sure' screen, and no 'undo' button. Once you delete these users they will be gone completely from your list. - As will their unsubscribe data, and activity history etc. Make sure this is the right action to take before you do this! Mailchimp offers a bulk unsubscribe feature which is safer. Also it might be worth <a href="http://kb.mailchimp.com/article/does-mailchimp-backup-my-data">backing up your data first</a>.</strong></p>
-<p><form action="bulk-delete.php?mcboogie=woogie" method="post"
-enctype="multipart/form-data">
-<label for="file">List of email addresses:</label>
-<input type="file" name="file" id="file" />
-<br />
-<label for="delete">Tick to acknowledge DELETION:</label><input type="checkbox" name="delete" value="true" /> - by ticking here you agree that you realise what you're doing.<br>
-<br />
-<label for="apikey">API Key to use</label><input type="text" name="apikey" />.<br>
-<br />
-<label for="listid">Unique ID for the list</label><input type="text" name="listid" />.<br>
-<br />
-<input type="submit" name="submit" value="DELETE" />
-</form></p>
-
-<?
-
-}
-// end form for uploading if nothing is uploaded
-
-?>
-<?
-include 'footer.php';
-?>
+              
+              
+echo "\n Processed ". (1000 * ($batch - 1) + $row);
